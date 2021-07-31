d19e21208744d6b4dafb2e621ff3b6942b5ef402
Ender Smith <ender@happyleviathan.com>
2021-07-31 08:48:11 -0700

    rules

--- src/helpers.js
+++ src/helpers.js
@@ -9,7 +9,7 @@ const renderLinkHeader = () => {
       <a href="resources.html">[Resources]</a>&nbsp;&nbsp;&nbsp;
     </span>
     <span>
-      <a href="rulebook.html">[Rulebook]</a>&nbsp;&nbsp;&nbsp;
+      <a href="./resources/rules/SabaccCorellianSpikeRules_v1.0.pdf">[Rulebook]</a>&nbsp;&nbsp;&nbsp;
     </span>
     <span>
       <a href="scorebot.html">[Score Calculator]</a>&nbsp;&nbsp;&nbsp;
